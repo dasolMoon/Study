eef63b3f69c42474b42002e96a5e5b8c35b1d041
Moon Dasol <mds9852369@gmail.com>
2020-05-16 12:48:21 +0900

    2020.05.16 k-means_m method update

--- k-means_m/k-means_m/Form1.cs
+++ k-means_m/k-means_m/Form1.cs
@@ -13,28 +13,32 @@ namespace k_means_m
     {
         Input input = null;
         Point[] p = null;
-        List<Point> pk= null;
+        List<Point> pList = null;
         List<List<Point>> lastClusters = null, nowClusters = null;
         int lastAvgk = 0, nowAvgk = 0; //이거안썼음
-        double lastDistance = 0, nowDistance = 0;
-        int lastChange = 0;
-        int k = 1;
+        double lastDistance = 0, lastChange = 0;
         List<Point> centroid = null;
+        bool re2 = true;
+        int k = 1;
         public Form1()
         {
             InitializeComponent();
         }
 
-        void init()
+        void init() //초기화작업
         {
-            //초기화작업
             nowClusters = new List<List<Point>>();
-            pk = new List<Point>();
+            for (int i = 0; i < 7; i++)
+            {
+                List<Point> temp = new List<Point>();
+                nowClusters.Add(temp);
+            }
+            pList = new List<Point>();
             centroid = new List<Point>();
 
             //데이터 가져오기
             getPoint();
-            for (int i = 0; i < p.Length; i++) pk.Add(p[i]);
+            for (int i = 0; i < p.Length; i++) pList.Add(p[i]);
 
             Console.WriteLine("초기화 단계 완료");
 
@@ -44,23 +48,40 @@ namespace k_means_m
             //초기화 작업 진행
             init();
 
-            //랜덤 클러스터 정하기
-            Random random = new Random();
-            int r = random.Next(0, pk.Count);
-            addCluster(pk[r]);
-            pk.RemoveAt(r);
-            Console.WriteLine("첫번째 단계 완료");
+            while (re2)// 랜덤 부여 ~ 변화비교
+            {
+                Console.WriteLine("**********전체 반복**********");//임시
+                //랜덤 클러스터 정하기
+                setK(k);
+                Console.WriteLine("첫번째 단계 완료");
+                bool re1 = true;
+                while (re1)//가까운 클러스터를 설정하고 새로운 centroid를 구함
+                {
+                    Console.WriteLine("**********부분 반복**********");//임시
+                    List<Point> lastCentroid = new List<Point>(centroid);
+                    //가장 가까운 클러스터 선정
+                    chooseCluster();
+                    Console.WriteLine("두번째 단계 완료");
 
-            //가장 가까운 클러스터 선정
-            chooseCluster();
-            Console.WriteLine("두번째 단계 완료");
+                    //클러스터 내부의 새로운 centroid를 구함
+                    inCluster();
+                    Console.WriteLine("세번째 단계 완료");
 
-            //클러스터 내부의 새로운 centroid를 구함
-            inCluster();
+                    bool isSame = true;
+                    for (int i = 0; i < centroid.Count; i++) //수정
+                    {
+                        if (lastCentroid[i] != centroid[i])
+                            isSame = false;
+                    }
+                    if (isSame == true)
+                        re1 = false;
+                    Console.WriteLine("Centroid값 조정 끝");//임시
+                }
 
-            //최고의 k를 구하기위한 검사
-            chooseK();
-            Console.WriteLine("n번째 단계 완료");
+                //최고의 k를 구하기위한 검사
+                chooseK();
+                Console.WriteLine("네번째 단계 완료");//임시
+            }
         }
         void getPoint() //포인트 배열을 가져옴
         {
@@ -68,9 +89,20 @@ namespace k_means_m
             p = input.getPoint();
 
         }
+
+        void setK(int item) //랜덤 클러스터 정하기
+        {
+            for (int i = 0; i < item; i++)
+            {
+                Random random = new Random();
+                int r = random.Next(0, pList.Count);
+                addCluster(pList[r]);
+                pList.RemoveAt(r);
+            }
+        }
         void addCluster(Point cent)
         {
-           centroid.Add(cent);
+            centroid.Add(cent);
         }
         double euclidean(Point a, Point b) //a와 b사이의 거리
         {
@@ -96,46 +128,76 @@ namespace k_means_m
                         minDistance = distance;
                     }
                 }
-                nowClusters[minIndex].Add(p[j]);// 아무것도 없어 ~~~~~~~~ 리스트가 초기화되어있지않다는 말임. 세부리스트에 넣고 이후에 넣을것
+                nowClusters[minIndex].Add(p[j]);
             }
         }
         void inCluster() //클러스터 안에서 새로운 centroid를 정함
         {
+            chek();//임시
             int sum_x = 0, sum_y = 0, count = 0;
-            Point newCentroid = new Point(0,0);
-            for(int i = 0; i<centroid.Count; i++)
+            Point newCentroid = new Point(0, 0);
+            for (int i = 0; i < centroid.Count; i++)
             {
-                for(int j = 0; j<nowClusters[i].Count; j++)
+                for (int j = 0; j < nowClusters[i].Count; j++)
                 {
                     sum_x += nowClusters[i][j].X;
                     sum_y += nowClusters[i][j].Y;
                     count++;
                 }
-                centroid[i] = new Point(sum_x / count, sum_y / count);
+                centroid[i] = new Point(sum_x / count, sum_y / count); //centroid 값을 조정했는데 하나가 만약에 0이 나오면 ? 지워야됨
             }
+            chek();//임시
+        }
 
+        void chek()
+        {
+            Console.WriteLine(" **클러스터 비교** " );
+            for (int i = 0; i<centroid.Count;i++)
+                Console.WriteLine(i+"번째 클러스터"+ centroid[0]);
         }
         void chooseK() //최고의 k를 구하기위한 검사 - 각 클러스터에서 좌표들과 centroid사이의 거리를 구하여 더한다
         {
+            double nowDistance = 0, change = 0;
             for (int i = 0; i < centroid.Count; i++)
             {
                 for (int j = 0; j < nowClusters[i].Count; j++)
                     nowDistance += euclidean(centroid[i], nowClusters[i][j]);
             }
 
-            double change = nowDistance - lastDistance;
+            change = nowDistance - lastDistance;
 
-            if (change < lastChange && lastClusters != null)
+            if (change <= lastChange && lastClusters != null)
             {
                 //최적의 k가 이미 등장했을 수 있다 !! 
                 //과거를 ,. 이용하는 것 ,. 
+                Console.WriteLine("이제 어쩌지!");
             }
             else
             {
                 //현재 클러스터 목록을 이전 클러스터로 적용 후 초기화
                 lastClusters = nowClusters;
-               // lastAvgk = nowAvgk;//이거안썼음
+                // lastAvgk = nowAvgk;//이거안썼음
                 lastDistance = nowDistance;
+                lastChange = change;
+
+                //초기화작업
+                nowClusters.Clear();
+                for (int i = 0; i < 7; i++)
+                {
+                    List<Point> temp = new List<Point>();
+                    nowClusters.Add(temp);
+                }
+
+                if (k == p.Length || pList.Count==0)
+                    re2 = false;
+                else
+                {
+                    k = k * 2;
+                    if (pList.Count < k)
+                    {
+                        k = pList.Count;
+                    }
+                }
             }
         }
     }
